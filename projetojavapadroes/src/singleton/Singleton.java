fd2c9922e3d4d658a5fbc48c90ddf09dae80df53
Paulo2991 <paulovaz38034@gmail.com>
2023-06-30 13:57:13 -0300

    Classes Lasy Holder e eager

--- projetojavapadroes/src/singleton/Singleton.java
+++ projetojavapadroes/src/singleton/Singleton.java
@@ -4,7 +4,15 @@ public class Singleton {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SingletonLasy singletonLasy = SingletonLasy.getInstancia();
+		System.out.println(singletonLasy);
+		singletonLasy = SingletonLasy.getInstancia();
+		System.out.println(singletonLasy);
+		
+		SingletonEager singletonEager = SingletonEager.getInstancia();
+		System.out.println(singletonEager);
+		singletonEager = SingletonEager.getInstancia();
+		System.out.println(singletonEager);
 	}
 
 }
